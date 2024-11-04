aa8d0b68889dd9da52b5240363aea8d25560ecf2
stewartmb <165910969+stewartmb@users.noreply.github.com>
2024-11-03 21:43:15 -0500

    Update create_bucket.py

--- create_bucket.py
+++ create_bucket.py
@@ -1,19 +1,20 @@
 import boto3
 
 def lambda_handler(event, context):
-    # Input (JSON)
-    nombre_bucket = event['body']['bucket']
-    
-    # Process
-    s3 = boto3.client('s3')
+    nombre_bucket = event["body"]["name"]
+
     try:
-        s3.create_bucket(Bucket=nombre_bucket)
+        s3 = boto3.client("s3")
+        s3.create_bucket(Bucket=nombre_bucket, ObjectOwnership="BucketOwnerPreferred")
+        s3.put_public_access_block(Bucket=nombre_bucket, PublicAccessBlockConfiguration={'BlockPublicAcls': False,'IgnorePublicAcls': False,'BlockPublicPolicy': False,'RestrictPublicBuckets': False})
+        s3.put_bucket_acl(ACL='public-read-write',Bucket=nombre_bucket)
         return {
-            'statusCode': 200,
-            'message': f'Bucket {nombre_bucket} created successfully.'
+            "statusCode":201,
+            "bucket": nombre_bucket,
         }
     except Exception as e:
+        print(e)
         return {
-            'statusCode': 400,
-            'error': str(e)
+            "statusCode": 400,
+            "message":"No se pudo crear el bucket"
         }
