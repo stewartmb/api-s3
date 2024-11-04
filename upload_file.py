d81509de171db92073c8d2331dd6ba31c3cc2b21
stewartmb <165910969+stewartmb@users.noreply.github.com>
2024-11-03 21:44:17 -0500

    Update upload_file.py

--- upload_file.py
+++ upload_file.py
@@ -1,23 +1,14 @@
-import boto3
 import base64
+import boto3
 
 def lambda_handler(event, context):
-    # Input (JSON)
-    nombre_bucket = event['body']['bucket']
-    nombre_directorio = event['body']['directory'] + '/'  # Ensure it ends with '/'
-    objeto_key = nombre_directorio + event['body']['filename']
-    file_content = base64.b64decode(event['body']['content'])  # Decoding the base64 content
-    
-    # Process
-    s3 = boto3.client('s3')
-    try:
-        s3.put_object(Bucket=nombre_bucket, Key=objeto_key, Body=file_content)
-        return {
-            'statusCode': 200,
-            'message': f'File {event["body"]["filename"]} uploaded to directory {nombre_directorio} in bucket {nombre_bucket} successfully.'
-        }
-    except Exception as e:
-        return {
-            'statusCode': 400,
-            'error': str(e)
-        }
+    bucket = event["body"]["bucket"]
+    filename = event["body"]["filename"]
+    folder = event["body"]["folder"]
+    base_64_str = event["body"]["file"]
+
+    s3 = boto3.client("s3")
+    s3.put_object(Bucket=bucket, Key= folder + "/" + filename, Body=base64.b64decode(base_64_str))
+    return {
+        "statusCode": 201
+    }
