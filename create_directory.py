b2bb9a83d3a900144dcc07e217908ba93a4470a8
stewartmb <165910969+stewartmb@users.noreply.github.com>
2024-11-03 20:47:07 -0500

    Update and rename create_bucket.py to create_directory.py

--- create_directory.py
+++ create_directory.py
@@ -3,14 +3,16 @@ import boto3
 def lambda_handler(event, context):
     # Input (JSON)
     nombre_bucket = event['body']['bucket']
+    nombre_directorio = event['body']['directory'] + '/'  # Ensuring it ends with '/'
     
     # Process
     s3 = boto3.client('s3')
     try:
-        s3.create_bucket(Bucket=nombre_bucket)
+        # Create an empty object with a key ending in '/'
+        s3.put_object(Bucket=nombre_bucket, Key=nombre_directorio)
         return {
             'statusCode': 200,
-            'message': f'Bucket {nombre_bucket} created successfully.'
+            'message': f'Directory {nombre_directorio} created in bucket {nombre_bucket} successfully.'
         }
     except Exception as e:
         return {
