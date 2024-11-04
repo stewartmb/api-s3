11d3adac61948f749119b70b56b20367d2805a69
stewartmb <165910969+stewartmb@users.noreply.github.com>
2024-11-03 20:50:45 -0500

    Update and rename delete_object.py to create_bucket.py

--- create_bucket.py
+++ create_bucket.py
@@ -3,15 +3,14 @@ import boto3
 def lambda_handler(event, context):
     # Input (JSON)
     nombre_bucket = event['body']['bucket']
-    objeto_key = event['body']['key']
     
     # Process
     s3 = boto3.client('s3')
     try:
-        s3.delete_object(Bucket=nombre_bucket, Key=objeto_key)
+        s3.create_bucket(Bucket=nombre_bucket)
         return {
             'statusCode': 200,
-            'message': f'Object {objeto_key} deleted from bucket {nombre_bucket} successfully.'
+            'message': f'Bucket {nombre_bucket} created successfully.'
         }
     except Exception as e:
         return {
